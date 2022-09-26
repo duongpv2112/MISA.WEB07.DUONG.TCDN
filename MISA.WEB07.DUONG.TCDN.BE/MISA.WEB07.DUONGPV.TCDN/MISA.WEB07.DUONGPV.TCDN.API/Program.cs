63c3f0e81e6befc7584f22f167b0856f1035c4a5
Phạm Việt Dương <duongpd12@gmail.com>
2022-09-26 17:18:07 +0700

    Create project

---
+++ MISA.WEB07.DUONG.TCDN.BE/MISA.WEB07.DUONGPV.TCDN/MISA.WEB07.DUONGPV.TCDN.API/Program.cs
@@ -0,0 +1,45 @@
+using MISA.WEB07.DUONGPV.TCDN.DL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+DatabaseContext.ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+
+// Add services to the container.
+
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin();
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyMethod();
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MISA.WEB07.DUONG.TCDN.BE/MISA.WEB07.DUONGPV.TCDN/MISA.WEB07.DUONGPV.TCDN.DL/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.WEB07.DUONGPV.TCDN.DL
+{
+    public class DatabaseContext
+    {
+        /// <summary>
+        /// Chuỗi kết nối đến database MySQL
+        /// </summary>
+        public static string ConnectionString;
+    }
+}
